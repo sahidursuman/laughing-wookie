6b3da07649e27fddfd4fad22989ef412292a097c
Naomi <naomi.freeman@yahoo.ca>
2014-02-12 16:39:29 -0500

    patched decorators. gravatar user friendship page failing

---
+++ app/decorators/user_friendship_decorator.rb
@@ -0,0 +1,29 @@
+class UserFriendshipDecorator < Draper::Decorator
+  delegate_all
+
+  decorates :user_friendship
+
+  def friendship_state
+  	model.state.titelize
+  end	
+
+  def sub_message
+  	case model.state
+  	when 'pending'
+  		"Do you really want to be friends with #{model.user.first_name}.? You're probably cooler than them anyway ..." 
+  	when 'accepted'
+  		"You are friends with #{model.user.first_name}."
+  	end	
+  end	
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+end
+
+#video suggested correct code is #{model.friend.first_name}. But really, who knows?
