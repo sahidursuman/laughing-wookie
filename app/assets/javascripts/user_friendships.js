c78010414ff6630093159be34d2cc309a846c4aa
Naomi <naomi.freeman@yahoo.ca>
2014-02-12 17:22:56 -0500

    Added ajax to friendship request.

---
+++ app/assets/javascripts/user_friendships.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+ $('#add-friendship').click(function(event) {
+ 	event.preventDefault();
+ 	var addFriendshipBtn = $(this);
+ 	$.ajax({
+ 		url: Routes.user_friendships_path({user_friendship: { friend_id: addFriendshipBtn.data('friendId') }}),
+ 		dataType: 'json',
+ 		type: 'POST',
+ 		success: function(e) {
+ 			addFriendshipBtn.hide();
+ 			$('#friend-status').html("<a href='#' class='btn btn-success'>Social Validation Requested")
+ 		}
+ 	});
+ });
+});
