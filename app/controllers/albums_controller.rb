0b9e3ecde5e452fb25da266f2a1612105ded532f
Naomi <naomi.freeman@yahoo.ca>
2014-03-18 18:45:30 -0400

    Set up albums controller. Scope to current_user

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,20 +1,36 @@
 class AlbumsController < ApplicationController
   before_action :set_album, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, only: [:create, :new, :edit, :update, :destroy]
+  before_filter :find_user
+  before_filter :find_album, only: [:edit, :update, :destroy, :show]
   # GET /albums
   # GET /albums.json
   def index
-    @albums = Album.all
+    @albums = user.albums.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @albums }
+    end
   end
 
   # GET /albums/1
   # GET /albums/1.json
   def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @album }
+    end
   end
 
   # GET /albums/new
   def new
-    @album = Album.new
+    @album = current_user.albums.new
+    
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @album }
+    end
   end
 
   # GET /albums/1/edit
@@ -24,7 +40,7 @@ class AlbumsController < ApplicationController
   # POST /albums
   # POST /albums.json
   def create
-    @album = Album.new(album_params)
+    @album = current_user.albums.new(params[:album])
 
     respond_to do |format|
       if @album.save
@@ -61,8 +77,20 @@ class AlbumsController < ApplicationController
     end
   end
 
+  def url_options
+    { profile_name: params[:profile_name] }.merge(super)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
+    def find_user
+      @user = User.find_by_profile_name(params[:profile_name])
+    end
+
+    def find_album
+      @album = current_user.albums.find(params[:id])
+    end
+
     def set_album
       @album = Album.find(params[:id])
     end
