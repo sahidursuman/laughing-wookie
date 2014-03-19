27f66026c211afec2202e16b0b488cf2224e7b23
Naomi <naomi.freeman@yahoo.ca>
2014-03-18 22:43:58 -0400

    Add helper method to show current photo in edit picture

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+	def bootstrap_paperclip_picture(form, paperclip_object)
+		if form.object.send("#{paperclip_object}?")
+			content_tag(:div, class: 'control-group') do
+				content_tag(:label, "Current #{paperclip_object.to_s.titleize}", class: 'control-label') + 
+				content_tag(:div, class: 'controls') do
+					image_tag form.object.send(paperclip_object).send(:url, :small)
+				end
+			end
+		end
+	end
 
 	def status_document_link(status)
 		if status.document && status.document.attachment?
