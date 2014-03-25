1e95423d22fc9bb0ed5e21984b8fb6f13244b7de
Naomi <naomi.freeman@yahoo.ca>
2014-03-25 12:09:36 -0400

    add deleted rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404 
   
   protected
 
@@ -23,5 +24,9 @@ class ApplicationController < ActionController::Base
   private
   def render_permission_error
     render file: 'public/permission_error', status: :error, layout: false
+  end
+
+  def render_404
+    render file: 'public/404', status: :not_found
   end
 end
