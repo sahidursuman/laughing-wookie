ffaaf5d6cfc6ded9dd05c2f3c4faa0581207f294
Naomi <naomi.freeman@yahoo.ca>
2014-03-25 12:05:36 -0400

    Fixed album js route. Added js sort to activity feed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,6 +29,17 @@ var addActivity = function(item) {
 
 	if (!found) {
 		window.loadedActivities.push(item);
+/* Sort array by when it was created */
+		window.loadedActivities.sort(function(a, b) {
+			var returnValue;
+			if (a.created_at > b.created_at)
+				returnValue = -1;
+			if (b.created > a.created_at)
+				returnValue = 1;
+			if (a.created_at == b.created_at)
+				returnValue = 0;
+			return returnValue;
+		});
 	}
 
 	return item;
@@ -37,7 +48,10 @@ var addActivity = function(item) {
 var renderActivities = function() {
 	var source = $('#activities-template').html();
 	var template = Handlebars.compile(source);
-	var html = template({activities: window.loadedActivities});
+	var html = template({
+		activities: window.loadedActivities, 
+		count: window.loadedActivities.length
+	});
 	var $activityFeedLink = $('li#activity-feed');
 
 	$activityFeedLink.addClass('dropdown');
@@ -61,6 +75,11 @@ var pollActivity = function() {
   	}
   })
 }
+
+/* add a static link to the Activities feed under the dropdown */
+Handlebars.registerHelper('activityFeedLink', function () {
+	return new Handlebars.SafeString(Routes.activities_path());
+});
 /* Handlebars helper, because conditional logic in handlebars on a view is difficult */
 Handlebars.registerHelper('activityLink', function () {
 	
@@ -77,12 +96,16 @@ Handlebars.registerHelper('activityLink', function () {
 			path = Routes.album_path(activity.profile_name, activity.targetable_id);
 			break;
 		case "picture":
-			path = Routes.album_picture_path(activity.profile_name, activity.targetable.album);
+			path = Routes.album_picture_path(activity.profile_name, activity.targetable.album_id, activity.targetable_id);
 			break;
 		case "userfriendship":
 			path = Routes.profile_path(activity.profile_name);
 			break;	
 	}
+
+	// if (activity.action === 'deleted') {
+	//	path = '#';
+	// }
 
 	/* what is contained in the activities dropdown */
 	html = "<li><a href='" + path + "'>" + this.profile_name + " " + this.action + " a " + linkText + ". </a></li>";
